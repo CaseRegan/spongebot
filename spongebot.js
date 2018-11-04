6a492b25d6ba5ff8d4374ee7032c6a4719664b56
Case Regan <cp_regan@coloradocollege.edu>
2018-11-04 00:52:11 -0600

    spongebot client

---
+++ spongebot.js
@@ -0,0 +1,52 @@
+var Discord = require('discord.io');
+var auth = require('./auth.json');
+var net = require('net')
+
+var socket = new net.Socket();
+socket.connect(3000, "localhost", function() {
+    console.log("Client: Connected to server");
+});
+
+var bot = new Discord.Client({
+    token: auth.token,
+    autorun: true
+});
+
+socket.on("data", function (data) {
+    data = JSON.parse(data);
+    message = data.response;
+    console.log("Response from server: %s", message);
+    socket.write(JSON.stringify({ response: "Data recieved" }));
+
+    spongebob_message = '';
+    for (var i = 0; i < message.length; i++) {
+        if (Math.random() > 0.5) {
+            spongebob_message += message.charAt(i).toUpperCase();
+        } else {
+            spongebob_message += message.charAt(i).toLowerCase();
+        }
+    }
+
+    bot.sendMessage({
+        to: 'general',
+        message: spongebob_message 
+    });
+});
+
+#bot.on('message', function (user, userID, channelID, message, evt) {
+#    if (user == 'kpzerg') {
+#        spongebob_message = '';
+#        for (var i = 0; i < message.length; i++) {
+#            if (Math.random() > 0.5) {
+#                spongebob_message += message.charAt(i).toUpperCase();
+#            } else {
+#                spongebob_message += message.charAt(i).toLowerCase();
+#            }
+#        }
+#
+#        bot.sendMessage({
+#            to: channelID,
+#            message: spongebob_message 
+#        });
+#    }
+#});
